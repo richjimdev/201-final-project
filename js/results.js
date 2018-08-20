64d73d2493eacc58e5527606c2424fe691b1c8ae
Jeremy Beck <jeremycbeck@outlook.com>
2018-08-20 16:27:34 -0700

    display staff result functioning

--- js/results.js
+++ js/results.js
@@ -1,5 +1,24 @@
 'use strict';
 
-var newResult = JSON.parse(localStorage.getItem('result'));
+var newResult = JSON.parse(localStorage.getItem('staffWinner'));
 
 console.log(newResult);
+
+var resultsNode = document.getElementById('resultsDisplay');
+
+if (newResult) {
+  var staffName = document.createElement('h1');
+  var staffImg = document.createElement('img');
+  var staffBio = document.createElement('p');
+  staffName.textContent = newResult.name;
+  staffImg.src = newResult.img;
+  staffBio.textContent = newResult.bio;
+  resultsNode.appendChild(staffName);
+  resultsNode.appendChild(staffImg);
+  resultsNode.appendChild(staffBio);
+} else {
+  var noResults = document.createElement('h1');
+  noResults.textContent = 'No results dummy';
+  resultsNode.appendChild(noResults);
+}
+
